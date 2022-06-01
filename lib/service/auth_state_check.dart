ebb9445cfc83e1eec24c66f80d44bc08d03e54d7
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-01 17:49:25 +0300

    Update auth_state_check.dart

--- lib/service/auth_state_check.dart
+++ lib/service/auth_state_check.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
 class AuthState {
+  //initialize firebaseAuth
   final auth = FirebaseAuth.instance;
+  // checks if the user is not null if null it will return false, else true
   bool check() {
     return auth.currentUser != null;
   }
 
-  getpage() {}
 }
